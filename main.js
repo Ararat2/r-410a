8abe199c75c6d994325f92b8074f931e7221aa31
Ararat2 <araratm@boomte.ch>
2022-05-19 22:14:10 +0400

    Order modal fixes

--- main.js
+++ main.js
@@ -2,12 +2,20 @@
 
 const orderModal = document.querySelector("#order-modal");
 const orderButton = document.querySelector("#order-button");
+const retailPurchaseButton = document.querySelector("#retail-purchase-button");
+const wholesalePurchaseButton = document.querySelector("#wholesale-purchase-button")
 const orderModalCloseButton = document.querySelector("#order-modal-close-button");
 
-orderButton.addEventListener("click", () => {
+const onOrderModalOpen = () => {
     orderModal.classList.add("d-flex");
     orderModal.classList.remove("d-none");
-});
+}
+
+orderButton.addEventListener("click", onOrderModalOpen);;
+
+retailPurchaseButton.addEventListener("click", onOrderModalOpen);
+
+wholesalePurchaseButton.addEventListener("click", onOrderModalOpen);
 
 orderModalCloseButton.addEventListener("click", () => {
     orderModal.classList.add("d-none");
